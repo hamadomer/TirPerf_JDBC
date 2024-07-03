4a6cd8dca2c4402fcab7ddb3f70703259bb2b929
Omar1H1 <omerhamad1o1@gmail.com>
2024-07-03 15:50:03 +0200

    create_db updated

--- create_db.sh
+++ create_db.sh
@@ -12,6 +12,25 @@ export PGPASSWORD=$DB_PASS
 
 psql -U $DB_USER -h $DB_HOST -p $DB_PORT -c "CREATE DATABASE $DB_NAME;"
 
+# Create the applicatif table
+psql -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -c "
+CREATE TABLE applicatif (
+    id SERIAL PRIMARY KEY,
+    intitule VARCHAR(100),
+    version VARCHAR(50),
+    fonctions VARCHAR(200)
+);"
+
+# Create the scenario table
+psql -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -c "
+CREATE TABLE scenario (
+    id SERIAL PRIMARY KEY,
+    description VARCHAR(500),
+    applicatif_id SMALLINT,
+    scenario_fonctions VARCHAR(200)
+    );"
+
+
 unset PGPASSWORD
 
 echo "Database '$DB_NAME' created successfully."
